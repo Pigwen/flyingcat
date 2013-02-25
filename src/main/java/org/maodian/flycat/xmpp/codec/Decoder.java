b01766e4f81998151ff618176614387914be3beb
Cole.Wen <sitereginfo@gmail.com>
2013-02-25 11:10:15 +0800

    remove Decoder.Container

--- src/main/java/org/maodian/flycat/xmpp/codec/Decoder.java
+++ src/main/java/org/maodian/flycat/xmpp/codec/Decoder.java
@@ -15,31 +15,12 @@
  */
 package org.maodian.flycat.xmpp.codec;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamReader;
-
-import org.maodian.flycat.xmpp.XmppNamespace;
-import org.maodian.flycat.xmpp.extensions.xep0030.QueryInfoCodec;
-import org.maodian.flycat.xmpp.extensions.xep0030.QueryItemCodec;
-import org.maodian.flycat.xmpp.extensions.xep0030.ServiceDiscovery;
 
 /**
  * @author Cole Wen
  *
  */
 public interface Decoder {
-  public static final Map<QName, Decoder> CONTAINER = new HashMap<QName, Decoder>() {
-    {
-      put(new QName(XmppNamespace.CLIENT_CONTENT, "iq"), new InfoQueryCodec());
-      put(new QName(XmppNamespace.BIND, "bind"), new BindCodec());
-      put(new QName(XmppNamespace.SESSION, "session"), new SessionCodec());
-      put(new QName(ServiceDiscovery.INFORMATION, "query"), new QueryInfoCodec());
-      put(new QName(ServiceDiscovery.ITEM, "query"), new QueryItemCodec());
-    }
-  };
-
   public Object decode(XMLStreamReader xmlsr);
 }
