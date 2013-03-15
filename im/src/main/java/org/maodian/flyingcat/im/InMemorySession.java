24ecf4c6bde2e6458ad7941edbca51e7a1f00cce
Cole Wen <wennnnke@gmail.com>
2013-03-15 10:54:47 +0800

    1.introduce shiro-guice module 2.refactor pom

--- im/src/main/java/org/maodian/flyingcat/im/InMemorySession.java
+++ im/src/main/java/org/maodian/flyingcat/im/InMemorySession.java
@@ -16,7 +16,6 @@
 package org.maodian.flyingcat.im;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
