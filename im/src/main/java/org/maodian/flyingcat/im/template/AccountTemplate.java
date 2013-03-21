c2344f4bc0d6a26fe27265f05e919bc2975fb2c1
Cole Wen <wennnnke@gmail.com>
2013-03-21 15:42:38 +0800

    throw appropriate error code when register fails

--- im/src/main/java/org/maodian/flyingcat/im/template/AccountTemplate.java
+++ im/src/main/java/org/maodian/flyingcat/im/template/AccountTemplate.java
@@ -16,9 +16,16 @@
 package org.maodian.flyingcat.im.template;
 
 
+import java.lang.invoke.MethodHandles;
+
+import org.maodian.flyingcat.im.IMException;
 import org.maodian.flyingcat.im.Type;
+import org.maodian.flyingcat.im.UserError;
 import org.maodian.flyingcat.im.Verb;
 import org.maodian.flyingcat.im.entity.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.query.Order;
@@ -31,11 +38,17 @@ import org.springframework.stereotype.Service;
 @Service
 @Domain(Type.PERSON)
 public class AccountTemplate extends AbstractTemplate {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   @Operation(Verb.CREATE)
   public void register(Account account) {
     MongoTemplate template = getMongoTemplate();
     template.indexOps(Account.class).ensureIndex(new Index("username", Order.ASCENDING).unique());
-    getMongoTemplate().insert(account);
+    try {
+      getMongoTemplate().insert(account);
+    } catch (DuplicateKeyException e) {
+      log.warn("The username [{}] has been occupied", account.getUsername());
+      throw IMException.wrap(e, UserError.DUPLICATED_USERNAME);
+    }
   }
 }
